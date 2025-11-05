16e054aa81adcf2f92dc941706d0040e636fbb6f
Srishti <iitjee202312345@gmail.com>
2025-11-05 17:59:30 +0000

    SSTF tak compiled

--- DiskScheduling/FCFS.cpp
+++ DiskScheduling/FCFS.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
-#include <fstream>
+#include <vector>
 using namespace std;
 
 int main() {
-    ofstream file("example.txt", ios::app); // open in append mode
-    file << "Appended line.\n";
-    file.close();
-    cout << "Line appended.\n";
+    vector<int> requests = {98, 183, 37, 122, 14, 124, 65, 67};
+    int head = 53, total = 0;
+
+    for (int r : requests) {
+        total += abs(head - r);
+        head = r;
+    }
+
+    cout << "Total FCFS head movement: " << total << endl;
     return 0;
 }
